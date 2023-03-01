80d32e76f43d11295a82e48453ff46794917ec12
slange-dev <74963785+slange-dev@users.noreply.github.com>
2023-03-01 21:15:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,19 +12,17 @@ REPODIR="$(cd "$(dirname "$0")"; pwd -P)"
 cd "$REPODIR";
 
 if ! is_app_installed tmux; then
-  printf "WARNING: \"tmux\" command is not found. \
-Install it first\n"
+  echo -e "WARNING: \"tmux\" command is not found. Install it first\n"
   exit 1
 fi
 
 if [ ! -e "$HOME/.tmux/plugins/tpm" ]; then
-  printf "WARNING: Cannot found TPM (Tmux Plugin Manager) \
- at default location: \$HOME/.tmux/plugins/tpm.\n"
+  echo -e "WARNING: Cannot found TPM (Tmux Plugin Manager) at default location: \$HOME/.tmux/plugins/tpm.\n"
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
 if [ -e "$HOME/.tmux.conf" ]; then
-  printf "Found existing .tmux.conf in your \$HOME directory. Will create a backup at $HOME/.tmux.conf.bak\n"
+  echo -e "Found existing .tmux.conf in your \$HOME directory. Will create a backup at $HOME/.tmux.conf.bak\n"
 fi
 
 cp -f "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak" 2>/dev/null || true
@@ -34,10 +32,10 @@ ln -sf .tmux/tmux.conf "$HOME"/.tmux.conf;
 # Install TPM plugins.
 # TPM requires running tmux server, as soon as `tmux start-server` does not work
 # create dump __noop session in detached mode, and kill it when plugins are installed
-printf "Install TPM plugins\n"
+echo -e "Install TPM plugins\n"
 tmux new -d -s __noop >/dev/null 2>&1 || true 
 tmux set-environment -g TMUX_PLUGIN_MANAGER_PATH "~/.tmux/plugins"
 "$HOME"/.tmux/plugins/tpm/bin/install_plugins || true
 tmux kill-session -t __noop >/dev/null 2>&1 || true
 
-printf "OK: Completed\n"
+echo -e "OK: Completed\n"
