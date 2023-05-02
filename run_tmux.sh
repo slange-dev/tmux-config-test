fae1ccd9fd11386cdf862d7b5ba8589c38232238
slange-dev <74963785+slange-dev@users.noreply.github.com>
2023-05-03 01:44:40 +0200

    Update run_tmux.sh

--- run_tmux.sh
+++ run_tmux.sh
@@ -10,22 +10,22 @@ function start_tmux() {
     # Check for existing Tmux session for the User by user name
     # If there is one: attach the existing one
     # If there is none: create a new one and then attach it
-    if [[ "$(command tmux ls | grep -o $sessionName)" == "$sessionName" ]]; then
+    if [[ "$(command tmux ls | grep -o "$sessionName")" == "$sessionName" ]]; then
 		# Session found and attach for the user
-		command tmux attach-session -t $sessionName
+		command tmux attach-session -t "$sessionName"
     else 
         # If it wasn't a session for the user then start a default(new)
-        command tmux new-session -s $sessionName -d
+        command tmux new-session -s "$sessionName" -d
 
         # Session template for user root
-        if [[ $sessionName == root ]]; then
+        if [[ "$sessionName" == root ]]; then
 
         # Template config for root
         . ~/.tmux/.tmux.template.conf
         fi
 
             # Check if term is set to use 256 colours
-            if [ $TERM=='*256color' ]; then
+            if [ $TERM == '*256color' ]; then
 				# Force tmux to using 256 colours
 				command tmux -2 -u attach-session -d
             else
